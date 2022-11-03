c8d6a658b9b577c4c7844ae35e5c6e47da0ff5ee
matt <madler@pdx.edu>
2022-11-02 19:29:50 -0700

    easier to read

--- hw3/02-url.js
+++ hw3/02-url.js
@@ -3,17 +3,18 @@ const port = process.env.PORT || 5001;
 
 const server = http.createServer((req, res) => {
   const routes = [
-    '/attributes?hello=world&lorem=ipsum',
-    '/items?first=1&second=2&third=3&fourth=4',
-    '/characters?spongebob=squarepants&patrick=star&sandy=cheeks',
+    "/attributes?hello=world&lorem=ipsum",
+    "/items?first=1&second=2&third=3&fourth=4",
+    "/characters?spongebob=squarepants&patrick=star&sandy=cheeks",
   ];
 
   // use the URL interface to work with URLs
   // source: https://developer.mozilla.org/en-US/docs/Web/API/URL
   let url = new URL(req.url, `http://${req.headers.host}`);
-
+  //create new params Object
+  let params = new URLSearchParams(url.search);
   let getRoutes = () => {
-    let result = '';
+    let result = "";
 
     routes.forEach(
       (elem) => (result += `<li><a href="${elem}">${elem}</a></li>`)
@@ -22,17 +23,22 @@ const server = http.createServer((req, res) => {
     return result;
   };
 
-  if (req.url === '/') {
+  if (req.url === "/") {
     let routeResults = getRoutes();
 
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.writeHead(200, { "Content-Type": "text/html" });
     res.write(`<h1>Exercise 02</h1>`);
 
     res.write(`<ul> ${routeResults} </ul>`);
   }
-
-  // Add your code here
-
+else if (url.pathname === "/attributes") {
+    params.forEach(function (value, key) {
+      tabl = `<td style='border: 5px solid black;'>${key}</td>
+              <td style='border: 5px solid black;'>${value}</td>
+                 `;
+      res.write(`<table style='border: 5px solid black;'> ${tabl} </table>`);
+    });
+  }
   res.end();
 });
 
